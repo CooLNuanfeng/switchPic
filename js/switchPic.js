6e21dcb535c01adae1da789704ce86ae9510645f
nuanfeng <1634898698@qq.com>
2014-07-07 18:17:58 +0800

    step_1

--- js/switchPic.js
+++ js/switchPic.js
@@ -1,4 +1,22 @@
 // JavaScript Document
 
 //数组数据，动画按钮,  展示形式
+$(function(){
+	
+	function Factory(options){
+		var switchPic = new SwitchPic();
+		switchPic.init(options);
+	}
+	
+	$.switchPic = $.fn.switchPic = Factory;
+	
+})
 
+
+function SwitchPic(){
+	
+	this.settings = {
+		width: 520,
+		height:394
+	}
+}
